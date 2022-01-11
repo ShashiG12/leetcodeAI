421b93e54940c6f56824e82896a7f50654668a53
ShashiG12 <34193029+ShashiG12@users.noreply.github.com>
2022-01-11 13:58:57 -0500

    added comments

--- MLmodel.py
+++ MLmodel.py
@@ -1,22 +1,24 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 import regex as re
-import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score
 from sklearn import svm
+from sklearn.svm import SVC
 
+# convert dataframe to csv and set to show all columns
 df = pd.read_csv('leetcodeData.csv')
 pd.set_option('display.max_columns', None)
+
+# remove rows with empty problem text
 discard = ["NOLEETCODE LINK", "PREMIUMLOCKED","Sorry"]
 df = df[~df.Text.str.contains('|'.join(discard))]
 df = df.reset_index(drop=True)
 
+# enumerate problem types in dataframe to be used by ML algorithm
+# e.g. Two-Pointers = 1, Heap = 2, Sliding window = 3
 typeDict = {}
 count = 0
 for i in range (0, len(df)):
@@ -27,11 +29,13 @@ for i in range (0, len(df)):
     else:
         df['Type'][i] = typeDict[df['Type'][i]]
 
-print(df.iloc[:100])
-#nltk.download('stopwords')
+# uncomment to download stopwords
+# nltk.download('stopwords')
+
+# create corpus array
 corpus = []
 
-
+# clean the problem text of each row of the dataframe
 for i in range(0, len(df)):
     pText = re.sub('[ ^ a - zA - Z]', ' ', df['Text'][i])
     pText = df['Text'][i]
@@ -42,21 +46,31 @@ for i in range(0, len(df)):
     pText = ' '.join(pText)
     corpus.append(pText)
 
+# Vectorize problem text
 cv = CountVectorizer(max_features = 50)
 X = cv.fit_transform(corpus).toarray()
 y = df.iloc[:, 2].values
-y=y.astype('int')
+y = y.astype('int')
 
-print(X[3])
+# split data into train and test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-classifier = GaussianNB()
-classifier.fit(X_train, y_train)
+# create SVM
+svc = svm.SVC(kernel='rbf', C=1000,gamma=.01).fit(X_train, y_train)
 
-y_pred_NB = classifier.predict(X_test)
-print(accuracy_score(y_test, y_pred_NB))
+# Grid search code to find optimal hyperparameters
 
-svc = svm.SVC(kernel='linear', C=1,gamma='auto').fit(X, y)
+# param_grid = {'kernel': ['linear', 'rbf', 'poly', 'sigmoid'],
+#               'gamma': [1, 0.1, 0.01, 0.001, 0.0001],
+#               'C': [0.1, 1, 10, 100, 1000]}
+#
+# grid = GridSearchCV(SVC(), param_grid, refit = True, verbose = 3)
+# grid.fit(X_train, y_train)
+# print(grid.best_params_)
+#
+# grid_predictions = grid.predict(X_test)
+# print(accuracy_score(y_test, grid_predictions))
+
+# predict testing data and display accuracy score
 y_predSVM = svc.predict(X_test)
-
-print(accuracy_score(y_predSVM, y_test))
+print(accuracy_score(y_test, y_predSVM))
--- Scrape.py
+++ Scrape.py
@@ -1,63 +1,94 @@
-#https://www.youtube.com/watch?v=XQgXKtPSzUI
 import time
 import urllib.error
-
-from bs4 import BeautifulSoup as soup  # HTML data structure
-from urllib.request import urlopen as uReq  # Web client
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uReq
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 import pandas as pd
 
+# github page with leetcode links sorted by their problem type
+# e.g. "two-pointer", "sliding window", etc.
 page_url = "https://github.com/wisdompeak/LeetCode"
 
-
+# request page with urllib
+# more info at #https://www.youtube.com/watch?v=XQgXKtPSzUI
 uClient = uReq(page_url)
-
-
 page_soup = soup(uClient.read(), "html.parser")
 uClient.close()
 
+# variable to store all the links on the retrieved website
 allLinks = page_soup.findAll("a")
-links = []
 
+# declares the browser to be used to open each link
+# using selenium here instead of urllib to allow for leetcode webpage to load before grabbing HTML
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 
+# to be arrays of dataframe
 pName = []
 pText = []
 pType = []
+
+# keeps track of the link being opened when running the program
 count = 0
+
+# iterate through every link in the github page
 for link in allLinks:
+    # keeps track of whether a leetcode link has been found inside of the current link
     foundLeet = False
+
+    # gets only the related github links from the original github page
     if "https://github.com/wisdompeak/LeetCode/tree/master" in link.get('href'):
+
+        # filters title links
         if(link.get('href').split("/")[len(link.get('href').split("/")) - 2] == "master"):
             continue
         else:
+
+            # added to make sure each dataframe column remains the same size
             print(len(pName), len(pText), len(pType))
+
             count += 1
+
+            # program progress (e.g. currently on link "1/1224")
             print(f'{count}/{len(allLinks)}' + " ")
+
+            # adds the problem name and type to the respective arrays
+            # retrieved from the text of each link
             pName.append(link.get('href').split("/")[len(link.get('href').split("/")) - 1])
             pType.append(link.get('href').split("/")[len(link.get('href').split("/")) - 2])
 
             try:
+                # opens the github page with the leetcode link
                 newLink = link.get('href')
                 uClient = uReq(newLink)
                 page_soup1 = soup(uClient.read(), "html.parser")
                 uClient.close()
+
+                # all the link on the new github page
                 allNewLinks = page_soup1.findAll('a')
+
             except urllib.error.HTTPError:
                 print("Page not found")
                 pText.append("NOTFOUND")
                 continue
+            # iterates through all the links on the new github page
             for link2 in allNewLinks:
+                # retrieves only the relevant leetcode link
                 if link2.string == "Leetcode Link":
                     foundLeet = True
+
+                    # opens an instance of firefox to retrieve the html from
+                    # the loaded leetcode page
                     driver.get(link2.get('href'))
                     time.sleep(3)
                     page_source = driver.page_source
                     page = soup(page_source, 'html.parser')
+
                     problemText = ""
                     for paragraph in page.findAll('p'):
                         problemText += paragraph.text
+
+                    # check for unhelpful problem text results and filters them
                     if "sign in" not in problemText:
                         if problemText == "" or "Sorry" in problemText:
                             print("EMPTY")
@@ -68,11 +99,13 @@ for link in allLinks:
                     else:
                         print("PREMIUMLOCKED")
                         pText.append("PREMIUMLOCKED")
-            if (foundLeet == False):
+            # if no leetcode link is found on the second github page
+            if not foundLeet:
                 print("NOLEETCODELINK")
                 pText.append("NOLEETCODELINK")
 
 
+# creates new dataframe from previously created columns
 df = pd.DataFrame({'Name': pName, 'Text': pText, 'Type': pType})
 df.to_csv('leetcodeData.csv', index=False, encoding='utf-8')
 print(df.head())
